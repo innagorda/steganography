8bd6c42796df444f184015963e26acb3cc289014
Inna <lagorda299@gmail.com>
2017-11-12 22:20:34 +0300

    fix loop over hide_image.cols
    https://stackoverflow.com/questions/47197834/cvexception-at-memory-location-while-looping-over-cvmat-object

--- stego.cpp
+++ stego.cpp
@@ -37,8 +37,8 @@ cv::Mat save_img_dim(cv::Mat image, int h, int w) {
 
 
 cv::Mat hide_image(cv::Mat image, cv::Mat hide_image) {
-	
-	int i_current = 0, j_current = 16;
+
+	int i_current = 0, j_current = 15;
 	int curr_bit = 0;
 
 	//if (image.rows * image.cols < hide_image.rows * hide_image.cols * 8 + 2)
@@ -46,11 +46,11 @@ cv::Mat hide_image(cv::Mat image, cv::Mat hide_image) {
 
 	image = save_img_dim(image, hide_image.cols, hide_image.rows);
 
-	for(int i = 0; i < hide_image.rows; i++)
-		for (int j = 0; j < hide_image.cols; j++)
+	for (int i = 0; i < hide_image.rows; i++)
+		for (int j = 0; j < hide_image.cols; j++) {
 			for (int bit = 0; bit < 8; bit++) {
 
-				if (j_current < image.cols - 1) 
+				if (j_current < image.cols - 1)
 					j_current++;
 				else {
 					i_current++;
@@ -60,22 +60,24 @@ cv::Mat hide_image(cv::Mat image, cv::Mat hide_image) {
 				//std::cout << (int)image.at<cv::Vec3b>(i_current, j_current)[0] << " " << (int)image.at<cv::Vec3b>(i_current, j_current)[1]
 				//	<< " " << (int)image.at<cv::Vec3b>(i_current, j_current)[2] << std::endl;
 
-				auto mask = get_mask<int>(8-bit, 1);
+				auto mask = get_mask<int>(7 - bit, 1);
 
 				for (int channel = 0; channel < 3; channel++) {
 
-					curr_bit = (hide_image.at<cv::Vec3b>(i, j)[channel] & mask) >> 8-bit;
-				//	std::cout << curr_bit;
-					//std::cout << " i=" << i << " j=" << j << " i_current=" << i_current << " j_current=" << j_current << std::endl;
+					curr_bit = (hide_image.at<cv::Vec3b>(i, j)[channel] & mask) >> 7 - bit;
+						//std::cout << curr_bit;
+						//std::cout << " i=" << i << " j=" << j << " i_current=" << i_current << " j_current=" << j_current << std::endl;
 					image.at<cv::Vec3b>(i_current, j_current)[channel] = image.at<cv::Vec3b>(i_current, j_current)[channel] & get_mask<int>(1, 7) | curr_bit;
 
 				}
 				//std::cout << std::endl;
 
-
-				//std::cout << (int)image.at<cv::Vec3b>(i_current, j_current)[0] << " " << (int)image.at<cv::Vec3b>(i_current, j_current)[1]
-				//	<< " " << (int)image.at<cv::Vec3b>(i_current, j_current)[2] << std::endl;
+				
 			}
+			if ((i == 0) && (j == 0))
+				std::cout << (int)hide_image.at<cv::Vec3b>(0, 0)[0] << " " << (int)hide_image.at<cv::Vec3b>(0,0)[1]	<< " " << (int)hide_image.at<cv::Vec3b>(0,0)[2] << std::endl
+				<< (std::bitset<8>)hide_image.at<cv::Vec3b>(0, 0)[0] << " " << (std::bitset<8>)hide_image.at<cv::Vec3b>(0, 0)[1] << " " << (std::bitset<8>)hide_image.at<cv::Vec3b>(0, 0)[2] << std::endl;
+	}
 	return image;
 }
 
@@ -105,16 +107,16 @@ cv::Mat get_image(cv::Mat image) {
 	
 	cv::Mat hide_image;
 	int hide_image_cols = 0, hide_image_rows = 0;
-	int i_current = 0, j_current = 16;
+	int i_current = 0, j_current = 15;
 	int curr_bit = 0;
 
 	get_img_dim(image, hide_image_cols, hide_image_rows);
 
-	hide_image = cv::Mat(hide_image_rows, hide_image_cols, CV_8U);
-	hide_image = cv::Mat::zeros(hide_image_rows, hide_image_cols, CV_8U);
-	//std::cout << (hide_image.at<cv::Vec3b>(671, 298)) << std::endl;
+	hide_image = cv::Mat(hide_image_rows, hide_image_cols, CV_8UC3);
+	hide_image = cv::Mat::zeros(hide_image_rows, hide_image_cols, CV_8UC3);
+
 	for (int i = 0; i < hide_image.rows; i++)
-		for (int j = 0; j < hide_image.cols; j++)
+		for (int j = 0; j < hide_image.cols; j++) {
 			for (int bit = 0; bit < 8; bit++) {
 
 				if (j_current < image.cols - 1)
@@ -126,12 +128,12 @@ cv::Mat get_image(cv::Mat image) {
 
 				//std::cout << (int)image.at<cv::Vec3b>(i_current, j_current)[0] << " " << (int)image.at<cv::Vec3b>(i_current, j_current)[1]
 				//	<< " " << (int)image.at<cv::Vec3b>(i_current, j_current)[2] << std::endl;
-				std::cout << i << " cols " << j << std::endl;
-				std::cout << (hide_image.at<cv::Vec3b>(i, j)) << std::endl;
+				//std::cout << i << " cols " << j << std::endl;
+				//std::cout << (hide_image.at<cv::Vec3b>(i, j)) << std::endl;
 				for (int channel = 0; channel < 3; channel++) {
-					
+
 					curr_bit = (image.at<cv::Vec3b>(i_current, j_current)[channel] & 1);
-				
+
 					hide_image.at<cv::Vec3b>(i, j)[channel] = (hide_image.at<cv::Vec3b>(i, j)[channel] << 1) | curr_bit;
 
 				}
@@ -141,6 +143,8 @@ cv::Mat get_image(cv::Mat image) {
 				//std::cout << (int)image.at<cv::Vec3b>(i_current, j_current)[0] << " " << (int)image.at<cv::Vec3b>(i_current, j_current)[1]
 				//	<< " " << (int)image.at<cv::Vec3b>(i_current, j_current)[2] << std::endl;
 			}
+			
+		}
 	return hide_image;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -174,10 +178,12 @@ int main() {
 	//cv::namedWindow("imgChanged", CV_WINDOW_AUTOSIZE);        // or CV_WINDOW_AUTOSIZE for a fixed size window matching the resolution of the image
 															// CV_WINDOW_AUTOSIZE is the default
 	
-	cv::namedWindow("imgFound", CV_WINDOW_AUTOSIZE);														
+	cv::namedWindow("imgFound", CV_WINDOW_AUTOSIZE);	
+	cv::namedWindow("imgHidden", CV_WINDOW_AUTOSIZE);
 	//cv::imshow("imgOriginal", imgOriginal);     // show windows
 	//cv::imshow("imgChanged", imgChanged);
 	cv::imshow("imgFound", imgFound);
+	cv::imshow("imgHidden", imgHidden);
 
 	cv::waitKey(0);                 // hold windows open until user presses a key
 
